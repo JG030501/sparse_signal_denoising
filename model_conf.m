b38c94c2848e6b0b97ba144b2a42fc779d6d6e75
yuliagrajewska <85734513+yuliagrajewska@users.noreply.github.com>
2022-08-09 10:20:06 -0400

    Add files via upload

---
+++ model_conf.m
@@ -0,0 +1,632 @@
+function models = model_conf()
+% Load multiple models
+% Output: 
+%   models: length equal to number of loaded models
+%   models{c}: model number c
+%   models{c}{1} = args: stores all model related arguments
+%   models{c}{2}: stores python filters in structure format
+%   models{c}{3}: store the filters in matlab cell array
+%
+
+    %% Begin
+    models = cell(0);
+    
+    %% Testing gradual structures
+    models = CNN_models(models);
+end
+
+%% Help functions
+
+function [model_sn, model] = model_loading_basic(args)
+    model_sn = strcat(args.model_name,'_L',num2str(args.L));
+    model_sn = strcat(model_sn, '_C1',num2str(args.C1),'_C2',num2str(args.C2));
+    if args.L == 4
+        model_sn = strcat(model_sn, '_C3',num2str(args.C3));
+    end
+    model_sn = strcat(model_sn,'_k', num2str(args.k1),'_k', num2str(args.k2),'_k',num2str(args.k3));
+    model_sn = strcat(model_sn, '_i',num2str(args.i, '%0.2d'));
+    model_sn = strcat(model_sn, '_l',num2str(args.l, '%0.2d'));
+    model_sn = strcat(model_sn, '_u',num2str(args.u, '%0.2d'));
+    model = load(strcat(model_sn,'.mat'));
+end
+
+function model_mc = model_loading(model_mc)
+
+    % load a pre-trained model
+    [model_mc{1}.model_sn, model_mc{2}] = model_loading_basic(model_mc{1});
+    
+    % extract filters
+    model_mc{3} = filters_extract(model_mc);
+    
+end
+
+%% CNN
+function models = CNN_models(models)
+
+% 
+% 
+
+
+
+%                     model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CNN';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 0; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+% 
+% 
+%                         model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 0; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%                             model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 1; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%                             model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 2; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%                             model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 3; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%                             model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 4; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%            model_temp = cell(3, 1);
+%   %  Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CNN_2';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 0; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+%            model_temp = cell(3, 1);
+  %  Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CNN_2';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 4; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+%            model_temp = cell(3, 1);
+%   %  Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v5';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 1; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+
+
+
+%                model_temp = cell(3, 1);
+%   %  Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v5';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 1; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+
+%                model_temp = cell(3, 1);
+%   %  Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v5';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 2; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%                model_temp = cell(3, 1);
+%   %  Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v5';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 3; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%             model_temp = cell(3, 1);
+%   %  Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v5';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 4; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+
+%                 model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v4';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 4; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%                 model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v1';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 2;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 2; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+% 
+% 
+%                     model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v3';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 2;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 1; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%                     model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v6';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 6;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 3; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+%             model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v3';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 2;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 2; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%             model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v6';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 6;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 =1;
+%     model_temp{1}.i = 3; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+%             model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v3';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 2;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 0; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+%             model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 1; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10;
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%                 model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 2; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+% 
+%     model_temp = cell(3, 1);
+%     %Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 3; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+% 
+%         model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 4; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+
+%      model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CNN_2';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 3; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+
+
+%          model_temp = cell(3, 1);
+%     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CNN_2';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 2; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%          model_temp = cell(3, 1);
+%     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v5';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 2; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+
+%              model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v5';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 4;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 3; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+%              model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_L4_v4';
+%     model_temp{1}.L = 4;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 4;
+%     model_temp{1}.C3 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 35;
+%     model_temp{1}.i = 4; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+
+% 
+%         model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 4; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%             model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 3; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%             model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 2; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% 
+%             model_temp = cell(3, 1);
+% %     Args
+%     model_temp{1} = struct;
+%     model_temp{1}.model_name = 'CCNN_v3';
+%     model_temp{1}.L = 3;
+%     model_temp{1}.C1 = 6;
+%     model_temp{1}.C2 = 2;
+%     model_temp{1}.k1 = 11;
+%     model_temp{1}.k2 = 35;
+%     model_temp{1}.k3 = 1;
+%     model_temp{1}.i = 1; 
+%     model_temp{1}.l = 10; 
+%     model_temp{1}.u = 10; 
+%     model_temp = model_loading(model_temp);
+%     models{end+1} = model_temp;
+% % 
+            model_temp = cell(3, 1);
+%     Args
+    model_temp{1} = struct;
+    model_temp{1}.model_name = 'CCNN_v3';
+    model_temp{1}.L = 3;
+    model_temp{1}.C1 = 6;
+    model_temp{1}.C2 = 2;
+    model_temp{1}.k1 = 11;
+    model_temp{1}.k2 = 35;
+    model_temp{1}.k3 = 1;
+    model_temp{1}.i = 0; 
+    model_temp{1}.l = 10; 
+    model_temp{1}.u = 10; 
+    model_temp = model_loading(model_temp);
+    models{end+1} = model_temp;
+                model_temp = cell(3, 1);
+%     Args
+    model_temp{1} = struct;
+    model_temp{1}.model_name = 'CNN';
+    model_temp{1}.L = 3;
+    model_temp{1}.C1 = 6;
+    model_temp{1}.C2 = 2;
+    model_temp{1}.k1 = 11;
+    model_temp{1}.k2 = 35;
+    model_temp{1}.k3 = 1;
+    model_temp{1}.i = 0; 
+    model_temp{1}.l = 10; 
+    model_temp{1}.u = 10; 
+    model_temp = model_loading(model_temp);
+    models{end+1} = model_temp;
+    end
+
